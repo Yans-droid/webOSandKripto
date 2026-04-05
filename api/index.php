1aba3767aafc21355733c968bd1c2c20ffb171cb
Yans-droid <riantoagustinu2000@gmail.com>
2026-04-05 23:31:28 +0700

    Force PHP runtime execution

--- api/index.php
+++ api/index.php
@@ -1,3 +1,10 @@
 <?php
-// Mengarahkan Vercel ke file index Laravel yang asli
-require __DIR__ . '/../public/index.php';
+require __DIR__ . '/../vendor/autoload.php';
+$app = require_once __DIR__ . '/../bootstrap/app.php';
+
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+$response = $kernel->handle(
+    $request = Illuminate\Http\Request::capture()
+);
+$response->send();
+$kernel->terminate($request, $response);
